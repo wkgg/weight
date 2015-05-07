e52369f51f1ad30bcefac647017da6237463b399
Amos <wky1991@gmail.com>
2015-05-07 22:10:50 +0800

    config puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,7 @@ set :repo_url, 'git@github.com:wkgg/weight.git'
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_bind, %w(tcp://0.0.0.0:9292 unix:///tmp/puma.sock)
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
 set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
