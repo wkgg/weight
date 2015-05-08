afabe5a55087963d79a732c3ab2ea74a0d3122b5
Amos <wky1991@gmail.com>
2015-05-08 11:10:53 +0800

    config puma

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ group :development, :test do
 
   gem 'pry'
 
-  gem 'capistrano-rails'  
+  gem 'capistrano-rails'
+  gem 'capistrano-rvm'  
   gem 'capistrano-bundler'
   gem 'capistrano3-puma'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,19 @@ set :repo_url, 'git@github.com:wkgg/weight.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :puma_socket, "unix:///var/run/weight.sock"
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "/var/run/pids/puma.state"
+set :puma_pid, "/var/run/pids/puma.pid"
+set :puma_bind, "unix:///var/run/weight.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_init_active_record, true
+set :puma_preload_app, true
 
 namespace :deploy do
 
